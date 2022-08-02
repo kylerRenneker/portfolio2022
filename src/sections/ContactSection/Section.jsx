187727e12ed78bfc3f3c2929e62d531dd0138207
Kyler Renneker <krenneker@peakactivity.com>
2022-08-02 12:53:20 -0400

    update contact section header

--- src/sections/ContactSection/Section.jsx
+++ src/sections/ContactSection/Section.jsx
@@ -5,7 +5,7 @@ import "./ContactSection.scss";
 const Contact = () => {
   return (
     <section id="contact" className="section-container section-pad-top">
-      <h3>0.4 What's Next?</h3>
+      <h3 className="mono">0.4 What's Next?</h3>
       <h2>Get In Touch</h2>
 
       <a href="mailto:krenneker16@gmail.com">Say Hello</a>
